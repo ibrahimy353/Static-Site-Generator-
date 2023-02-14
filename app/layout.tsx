1d74183cd04a2cc57e25bec637af6a44835a4f5b
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-14 18:59:47 +0300

    added changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,40 @@
-import './globals.css'
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  const header = (
+    <header>
+      <div>
+        <h1> Ibrahim SSG</h1>
+        <p>Welcome to my static site generator</p>
+        <br/>
+      </div>
+    </header>
+  );
+
+
+const footer = (
+  <footer>
+    <div>
+      <br/>
+      <h3>Devloped by Ibra</h3>
+    </div>
+  </footer>
+)
+
   return (
     <html lang="en">
       {}
       <head />
       
       <body>
-        <header> this header </header>
-        {children}</body>
-        <footer> this is footer</footer>
+        {header}
+        {children}
+        {footer}
+        </body>
     </html>
   )
 }

8480ab22fd8ed9679ec9fce565c3b70508d5c482
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-15 19:03:19 +0300

    added grib column for different screen sizes

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const Homepage = () => {
     <PostPreview key= {post.slug} {...post} />
   ));
 
-  return <div>{postPreviews}</div>;
+  return <div className=" grid grid-cols-1 md:grid-cols-2 gap-4">{postPreviews}</div>;
 };
 
 export default Homepage;

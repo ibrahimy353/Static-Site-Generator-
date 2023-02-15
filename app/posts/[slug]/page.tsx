5178193f64c88d41eeb8dd00988044978fcb7f6a
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-15 14:42:34 +0300

    added markdown plugin to convert md into html

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,21 +1,22 @@
 import fs from "fs";
+import Markdown from "markdown-to-jsx";
 
 const getPostContent = (slug: string) => {
   const folder ='posts/';
   const file = `${folder}${slug}.md`;
-  const content = fs.readFileSync( file, "utf-8");
+  const content = fs.readFileSync( file, "utf-8"); //file path with utf encoding
   return content;
 
 };
 
 const PostPage = (props: any) => {
-  const slug = props.params.slug;
+  const slug = props.params.slug;//the .slug can be any name that u have used in the square brackets
   const content = getPostContent(slug);
   return (
-    <p>
+    <div>
       <h1>This is a post: {slug}</h1>
-      <p>{content}</p>
-    </p>
+      <Markdown>{content}</Markdown>
+    </div>
   );
 };
  export default PostPage;

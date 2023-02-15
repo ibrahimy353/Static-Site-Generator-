ff2ca14c7a50e0db3c20f68ce0be2fdef5dc3e22
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-15 14:35:36 +0300

    added function that enables us to see the content of the md

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,8 +1,20 @@
+import fs from "fs";
+
+const getPostContent = (slug: string) => {
+  const folder ='posts/';
+  const file = `${folder}${slug}.md`;
+  const content = fs.readFileSync( file, "utf-8");
+  return content;
+
+};
+
 const PostPage = (props: any) => {
   const slug = props.params.slug;
+  const content = getPostContent(slug);
   return (
     <p>
       <h1>This is a post: {slug}</h1>
+      <p>{content}</p>
     </p>
   );
 };

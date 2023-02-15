8c4c928d141ed06524488555a8ec0a4e510c7865
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-15 16:36:20 +0300

    modify

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -7,17 +7,17 @@ const getPostContent = (slug: string) => {
   const file = `${folder}${slug}.md`;
   const content = fs.readFileSync( file, "utf-8"); //file path with utf encoding
   const matterResult = matter(content);
-  return matterResult.content;
+  return matterResult;
 
 };
 
 const PostPage = (props: any) => {
   const slug = props.params.slug;//the .slug can be any name that u have used in the square brackets
-  const content = getPostContent(slug);
+  const post= getPostContent(slug);
   return (
     <div>
-      <h1>This is a post: {slug}</h1>
-      <Markdown>{content}</Markdown>
+      <h1>{post.data.title}</h1>
+      <Markdown>{post.content}</Markdown>
     </div>
   );
 };

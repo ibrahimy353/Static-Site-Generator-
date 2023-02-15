20f8ffb13fd9f86bcdb5d0a845d3d1d5c84ad740
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-15 17:47:45 +0300

    modify

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,11 @@
-import  fs  from "fs";
+import getPostMetadata from "@/components/getPostMetadata";
+import PostPreview from "@/components/PostPreview";
 import Link from "next/link";
-import matter from "gray-matter";
-import { PostMetadata } from "../components/PostMetadata";
-
-const getPostMetadata = (): PostMetadata[] =>{
-  const folder = "posts/";
-  const files = fs.readdirSync(folder);
-  const markdownPosts = files.filter ((file) => file.endsWith (".md"));//gets to select the files that endwith .md
-  
-  //get gray-matter data from each file.
-  const posts = markdownPosts.map((fileName) => {
-    const fileContents = fs.readFileSync(`posts/${fileName}`, "utf-8");
-    const matterResult = matter(fileContents);
-    return {
-      title : matterResult.data.title,
-      date: matterResult.data.date,
-      subtitle: matterResult.data.subtitle,
-      slug: fileName.replace(".md", ""),
-    };
-  });
-   
-  return posts;
-
-};
 
 const Homepage = () => {
   const postMetadata = getPostMetadata();
   const postPreviews = postMetadata.map((post) => (
-    <div>
-      <Link href={`/posts/${post.slug}`}> 
-      <h1>{post.title}</h1> 
-      </Link>
-      <p>{post.subtitle}</p>
-      <p>{post.date}</p>
-
-
-     
-      {/* added link that connects the homepage with the md files */}
-    </div>
+    <PostPreview key= {post.slug} {...post} />
   ));
 
   return <div>{postPreviews}</div>;
--- posts/javascript-performance-tips.md
+++ posts/javascript-performance-tips.md
@@ -1,7 +1,7 @@
 ---
 title: 'JavaScript Performance Tips'
 date: 'March 4, 2021'
-excerpt: 'We will look at 10 simple tips and tricks to increase the speed of your code when writing JS'
+subtitle: 'We will look at 10 simple tips and tricks to increase the speed of your code when writing JS'
 cover_image: '/images/posts/img1.jpg'
 ---
 

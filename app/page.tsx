b8c149f07a9438970ac8e9642705d79c66802448
ibrahimy353 <ibrahimy353@gmail.com>
2023-02-15 15:21:47 +0300

    added  gray mater fr changing the tittle signs -- in  heading

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 import  fs  from "fs";
 import Link from "next/link";
+import matter from "gray-matter";
 
 const getPostMetadata = () =>{
   const folder = "posts/";
   const files = fs.readdirSync(folder);
   const markdownPosts = files.filter ((file) => file.endsWith (".md"));//gets to select the files that endwith .md
-  const slugs = markdownPosts.map((file) => file.replace (".md", ""));
-  return slugs;
+  
+  //get gray-matter data from each file.
+  const post = markdownPosts.map((fileName) => {
+    const fileContents = fs.readFileSync(`post/${fileName}`, "utf-8");
+    const matterResult = matter(fileContents);
+    return {
+      title : matterResult.data.title,
+      date: matterResult.data.date,
+      subtitle: matterResult.data.subtitle,
+      slug: __filename.replace(".md", ""),
+    };
+  });
 };
 
 const Homepage = () => {

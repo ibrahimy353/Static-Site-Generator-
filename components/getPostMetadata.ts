10d1154db9487139b8e123d6e43be954aeacbcf1
ibrahimy353 <ibrahimy353@gmail.com>
2023-03-17 16:55:57 +0300

    changes

--- components/getPostMetadata.ts
+++ components/getPostMetadata.ts
@@ -2,13 +2,12 @@ import  fs  from "fs";
 import matter from "gray-matter";
 import { PostMetadata } from "../components/PostMetadata";
 
-
 const getPostMetadata = (): PostMetadata[] =>{
     const folder = "posts/";
-    const files = fs.readdirSync(folder);
-    const markdownPosts = files.filter ((file) => file.endsWith (".md"));//gets to select the files that endwith .md
-    
-    //get gray-matter data from each file.
+// fs.readdirSync synchronously gets to read file content of files ending with .md
+    const files = fs.readdirSync(folder);  
+    const markdownPosts = files.filter ((file) => file.endsWith (".md"));
+    // get to return content and encode from md language to utf language. 
     const posts = markdownPosts.map((fileName) => {
       const fileContents = fs.readFileSync(`posts/${fileName}`, "utf-8");
       const matterResult = matter(fileContents);
@@ -16,14 +15,9 @@ const getPostMetadata = (): PostMetadata[] =>{
         title : matterResult.data.title,
         date: matterResult.data.date,
         subtitle: matterResult.data.subtitle,
-        slug: fileName.replace(".md", ""),
-         //content being returned from file [slugs]  after being encoded to Utf-8 which can be posted to html static.
-      
+        slug: fileName.replace(".md", ""), //content being returned from file [slugs]  after being encoded to Utf-8 which can be posted to html static.
       };
-    });
-     
+    }); 
     return posts;
-  
   };
-
   export default getPostMetadata;
